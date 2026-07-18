99dd5f4171cf6f63f43913c6cf37d1013e224457
Boss <boss@example.com>
2026-07-18 16:30:38 +0530

    Added MainActivity

---
+++ app/src/main/java/com/bossnyra/MainActivity.java
@@ -0,0 +1,8 @@
+ackage com.bossnyra;
+
+public class MainActivity {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BossNyra!");
+        System.out.println("Boss + Nyra is starting...");
+    
