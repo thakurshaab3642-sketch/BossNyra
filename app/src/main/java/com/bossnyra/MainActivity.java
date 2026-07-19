0827fdafb9cd462b7638f5fdb3b25d42cb9cc936
thakurshaab3642-sketch <thakurshaab3642@gmail.com>
2026-07-19 13:40:47 +0530

    Update MainActivity.java

--- app/src/main/java/com/bossnyra/MainActivity.java
+++ app/src/main/java/com/bossnyra/MainActivity.java
@@ -1,8 +1,9 @@
-ackage com.bossnyra;
+package com.bossnyra;
 
 public class MainActivity {
 
     public static void main(String[] args) {
         System.out.println("Welcome to BossNyra!");
         System.out.println("Boss + Nyra is starting...");
-    
+    }
+}
